0cb7b9523ac1ca0d4ed5e79e984a5a273f643c00
6en6ar <6eng4r@gmail.com>
2022-08-10 15:13:45 +0200

    open in browser

--- gork.go
+++ gork.go
@@ -6,15 +6,25 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"os/exec"
+	"runtime"
 )
 
 var gurl string
 
-func GenerateLink(dork string) {
+func GenerateLink(dork string, domain string) {
 	gurl := "https://www.google.com/search?q=" + url.QueryEscape(dork)
 	// write to .html
-	fmt.Println(gurl)
-	fmt.Println("-------------------------------------------------------------------")
+	f, err := os.OpenFile(domain+".html", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("[ - ] Error opening file..")
+	}
+	if _, err := f.Write([]byte("<li><a href=\"" + gurl + "\" target=\"_blank\"" + ">" + gurl + "</a></li>")); err != nil {
+		fmt.Println("[ - ] Error writing to file..")
+	}
+	if err := f.Close(); err != nil {
+		fmt.Println("[ - ] Error closing file..")
+	}
 
 }
 func ReadDorks(domain string) {
@@ -27,8 +37,16 @@ func ReadDorks(domain string) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		dork := scanner.Text() + " site:" + domain
-		GenerateLink(dork)
+		GenerateLink(dork, domain)
 	}
+
+}
+func CreateHtml(domain string) {
+	f, err := os.Create(domain + ".html")
+	if err != nil {
+		fmt.Println("[ - ] Error creating file")
+	}
+	defer f.Close()
 
 }
 
@@ -62,5 +80,16 @@ func main() {
 	fmt.Printf("Gorking...")
 	fmt.Println("hey")
 	fmt.Println(banner)
+	CreateHtml(*domain)
+	fmt.Println("[ + ] File " + *domain + ".html Created")
 	ReadDorks(*domain)
+	var env = runtime.GOOS
+	fmt.Println("[ + ] Opening file in your browser ...")
+	switch env {
+	case "linux":
+		_ = exec.Command("/usr/bin/xdg-open", *domain+".html").Start()
+	case "windows":
+		_ = exec.Command("C:\\Windows\\System32\\rundll32", "url.dll,FileProtocolHandler", *domain+".html").Start()
+	}
+
 }
